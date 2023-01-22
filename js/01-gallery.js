1d03fd8463d7957dbe704d8e1550fea4414db236
Vitalii111 <32221560+Vitalii111@users.noreply.github.com>
2023-01-22 17:47:49 +0200

    confirm

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+
+function addGallery(image) {
+  return image
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery-item">
+    <a class="gallery__link" href="${original}">
+    <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    />
+    </a>
+    </div>`;
+    })
+    .join("");
+}
+
+const imageItemsMarkup = addGallery(galleryItems);
+
+galleryRef.innerHTML = imageItemsMarkup;
+
+galleryRef.addEventListener("click", onImageClick);
+
+function onImageClick(e) {
+  blockStandartAction(e);
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+<img src="${e.target.dataset.source}" width="800" height="600">`);
+  instance.show();
+
+  galleryRef.addEventListener("keydown", (e) => {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  });
+}
+
+function blockStandartAction(e) {
+  e.preventDefault();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,51 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+
+function addGallery(image) {
+  return image
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery-item">
+    <a class="gallery__link" href="${original}">
+    <img
+    class="gallery__image"
+    src="${preview}"
+    alt="${description}"
+    />
+    </a>
+    </div>`;
+    })
+    .join("");
+}
+
+const imageItemsMarkup = addGallery(galleryItems);
+
+galleryRef.innerHTML = imageItemsMarkup;
+
+galleryRef.addEventListener("click", onImageClick);
+
+function onImageClick(e) {
+  blockStandartAction(e);
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  let lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionsDelay: 250,
+    scrollZoom: false,
+  });
+
+  if (e.target.nodeName !== "IMG") {
+    close.SimpleLightbox;
+  }
+}
+
+function blockStandartAction(e) {
+  e.preventDefault();
+}
